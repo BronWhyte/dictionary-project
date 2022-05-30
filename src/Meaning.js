9eea6f74b23db3b93d728657a6256d56475224c1
BronWhyte <bronwyn.whyte24@gmail.com>
2022-05-30 19:10:00 +0100

    Added ternary statement for examples and added Synonyms component.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <p>Definition: {definition.definition}</p>
             <br />
-            <p>{definition.example}</p>
+            {definition.example ? <p>Example: {definition.example}</p> : null}
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
